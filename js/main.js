f08d4cba1ae76381f3a5f9668755aff403c41c27
lilia <lilia.mihalache@gmail.com>
2021-08-13 22:25:14 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function show(){
     document.getElementById("side-menu").classList.toggle("show")
 }
+
+function data(){
+    var span;
+    var date = new Date();
+
+    an = date.getFullYear();
+
+    span = document.getElementById("data");
+
+    span.innerHTML ="@ Comisia metodică MaIF - L.T. „Ion Creangă” <br/>" + an;
+}
+
+data();
